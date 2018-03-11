4c5a602fb68f9c673224682c73699bcd1364203c
YoshihitoAso <yoshihito.asoh@gmail.com>
2018-03-11 15:51:03 +0900

    token_addressのValidationチェックを追加

--- app/api/v1/marketInformation.py
+++ app/api/v1/marketInformation.py
@@ -216,4 +216,8 @@ class Tick(BaseResource):
         if not validator.validate(request_json):
             raise InvalidParameterError(validator.errors)
 
+        for token_address in request_json['address_list']:
+            if not Web3.isAddress(token_address):
+                raise InvalidParameterError
+
         return request_json
