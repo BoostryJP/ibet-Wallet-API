0570996e5ccd414b8b3f5be82dcaa38c48256b6a
YoshihitoAso <yoshihito.asoh@gmail.com>
2018-03-12 16:07:54 +0900

    不要Configの削除

--- app/config.py
+++ app/config.py
@@ -2,14 +2,8 @@
 
 import os
 import configparser
-from itertools import chain
 
 BRAND_NAME = 'TMR-API'
-
-SECRET_KEY = os.environ.get('SECRET_KEY') or 'xs4G5ZD9SwNME6nWRWrK_aq6Yb9H8VJpdwCzkTErFPw='
-UUID_LEN = 10
-UUID_ALPHABET = ''.join(map(chr, range(48, 58)))
-TOKEN_EXPIRES = 3600
 
 APP_ENV = os.environ.get('APP_ENV') or 'local'
 INI_FILE = os.path.join(
