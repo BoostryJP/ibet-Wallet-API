53b8cd4c327848b9bb503feeb13b210c7fede8e1
YoshihitoAso <yoshihito.asoh@gmail.com>
2018-04-01 22:56:33 +0900

    不要モジュールの削除

--- tmp/pk.js
+++
@@ -1,9 +0,0 @@
-var keythereum = require("keythereum");
-var datadir = "/home/yoshihito/eth_test_net";
-var address= "0x590acc995c350dc21f92523b8937ce32f879e65a";
-const password = "password";
-
-var keyObject = keythereum.importFromFile(address, datadir);
-var privateKey = keythereum.recover(password, keyObject);
-console.log(privateKey.toString('hex'));
-
--- tmp/replace.py
+++
@@ -1,9 +0,0 @@
-#coding:utf-8
-
-f = open('MyCoin.sol','r')
-Allf = f.read()
-text = Allf.replace('\n','')
-text = text.replace('\r','')
-print(text)
-
-f.close()
