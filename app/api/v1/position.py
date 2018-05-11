62a64e0d89f66798ec792e0a6e6046a6e63266de
YoshihitoAso <yoshihito.asoh@gmail.com>
2018-05-11 19:02:52 +0900

    apply yapf

--- app/api/v1/position.py
+++ app/api/v1/position.py
@@ -17,6 +17,7 @@ from app import config
 
 LOG = log.get_logger()
 
+
 # ------------------------------
 # 保有トークン一覧
 # ------------------------------
@@ -24,6 +25,7 @@ class MyTokens(BaseResource):
     '''
     Handle for endpoint: /v1/MyTokens/
     '''
+
     def on_post(self, req, res):
         LOG.info('v1.Position.MyTokens')
         session = req.context['session']
@@ -36,8 +38,8 @@ class MyTokens(BaseResource):
         list_contract_address = os.environ.get('TOKEN_LIST_CONTRACT_ADDRESS')
         list_contract_abi = json.loads(config.TOKEN_LIST_CONTRACT_ABI)
         ListContract = web3.eth.contract(
-            address = to_checksum_address(list_contract_address),
-            abi = list_contract_abi,
+            address=to_checksum_address(list_contract_address),
+            abi=list_contract_abi,
         )
 
         try:
@@ -46,11 +48,12 @@ class MyTokens(BaseResource):
             company_list = []
 
         # Exchange Contract
-        exchange_contract_address = os.environ.get('IBET_SB_EXCHANGE_CONTRACT_ADDRESS')
+        exchange_contract_address = os.environ.get(
+            'IBET_SB_EXCHANGE_CONTRACT_ADDRESS')
         exchange_contract_abi = json.loads(config.IBET_EXCHANGE_CONTRACT_ABI)
         ExchangeContract = web3.eth.contract(
-            address = to_checksum_address(exchange_contract_address),
-            abi = exchange_contract_abi,
+            address=to_checksum_address(exchange_contract_address),
+            abi=exchange_contract_abi,
         )
 
         position_list = []
@@ -60,15 +63,18 @@ class MyTokens(BaseResource):
             try:
                 event_filter = ExchangeContract.eventFilter(
                     'Agree', {
-                        'filter':{'buyAddress':to_checksum_address(buy_address)},
-                        'fromBlock':'earliest'
-                    }
-                )
+                        'filter': {
+                            'buyAddress': to_checksum_address(buy_address)
+                        },
+                        'fromBlock': 'earliest'
+                    })
                 entries = event_filter.get_all_entries()
                 for entry in entries:
                     portfolio_list.append({
-                        'account':entry['args']['buyAddress'],
-                        'token_address':entry['args']['tokenAddress'],
+                        'account':
+                        entry['args']['buyAddress'],
+                        'token_address':
+                        entry['args']['tokenAddress'],
                     })
             except:
                 portfolio_list = []
@@ -76,27 +82,30 @@ class MyTokens(BaseResource):
             # リストをユニークにする
             portfolio_list_uniq = []
             for portfolio in portfolio_list:
-                if portfolio not in portfolio_list_uniq :
+                if portfolio not in portfolio_list_uniq:
                     portfolio_list_uniq.append(portfolio)
 
             # 残高（balance）、残注文（commitment）を取得する
             token_template = None
             for mytoken in portfolio_list_uniq:
                 token_address = to_checksum_address(mytoken['token_address'])
-                token_template = ListContract.functions.getTokenByAddress(token_address).call()
-                if token_template[0] == '0x0000000000000000000000000000000000000000':
+                token_template = ListContract.functions.getTokenByAddress(
+                    token_address).call()
+                if token_template[
+                        0] == '0x0000000000000000000000000000000000000000':
                     continue
 
-                abi_str = session.query(TokenTemplate).filter(TokenTemplate.template_name == token_template[1]).first().abi
+                abi_str = session.query(TokenTemplate).filter(
+                    TokenTemplate.template_name == token_template[
+                        1]).first().abi
                 token_abi = json.loads(abi_str)
                 TokenContract = web3.eth.contract(
-                    address = token_address,
-                    abi = token_abi
-                )
+                    address=token_address, abi=token_abi)
 
                 owner = to_checksum_address(mytoken['account'])
                 balance = TokenContract.functions.balanceOf(owner).call()
-                commitment = ExchangeContract.functions.commitments(owner,token_address).call()
+                commitment = ExchangeContract.functions.commitments(
+                    owner, token_address).call()
 
                 # 残高、残注文がゼロではない場合、Token-Contractから情報を取得する
                 if balance == 0 and commitment == 0:
@@ -106,42 +115,53 @@ class MyTokens(BaseResource):
                     symbol = TokenContract.functions.symbol().call()
                     totalSupply = TokenContract.functions.totalSupply().call()
                     faceValue = TokenContract.functions.faceValue().call()
-                    interestRate = TokenContract.functions.interestRate().call()
+                    interestRate = TokenContract.functions.interestRate().call(
+                    )
 
-                    interestPaymentDate_string = TokenContract.functions.interestPaymentDate().call()
+                    interestPaymentDate_string = TokenContract.functions.interestPaymentDate(
+                    ).call()
                     interestPaymentDate = json.loads(
-                        interestPaymentDate_string.replace("'", '"').replace('True', 'true').replace('False', 'false'))
+                        interestPaymentDate_string.replace("'", '"').replace(
+                            'True', 'true').replace('False', 'false'))
 
                     interestPaymentDate1 = ''
                     interestPaymentDate2 = ''
                     if 'interestPaymentDate1' in interestPaymentDate:
-                        interestPaymentDate1 = interestPaymentDate['interestPaymentDate1']
+                        interestPaymentDate1 = interestPaymentDate[
+                            'interestPaymentDate1']
                     if 'interestPaymentDate2' in interestPaymentDate:
-                        interestPaymentDate2 = interestPaymentDate['interestPaymentDate2']
+                        interestPaymentDate2 = interestPaymentDate[
+                            'interestPaymentDate2']
 
-                    redemptionDate = TokenContract.functions.redemptionDate().call()
-                    redemptionAmount = TokenContract.functions.redemptionAmount().call()
+                    redemptionDate = TokenContract.functions.redemptionDate(
+                    ).call()
+                    redemptionAmount = TokenContract.functions.redemptionAmount(
+                    ).call()
                     returnDate = TokenContract.functions.returnDate().call()
-                    returnAmount = TokenContract.functions.returnAmount().call()
+                    returnAmount = TokenContract.functions.returnAmount().call(
+                    )
                     purpose = TokenContract.functions.purpose().call()
-                    image_url_small = TokenContract.functions.getImageURL(0).call()
-                    image_url_medium = TokenContract.functions.getImageURL(1).call()
-                    image_url_large = TokenContract.functions.getImageURL(2).call()
+                    image_url_small = TokenContract.functions.getImageURL(
+                        0).call()
+                    image_url_medium = TokenContract.functions.getImageURL(
+                        1).call()
+                    image_url_large = TokenContract.functions.getImageURL(
+                        2).call()
                     owner_address = TokenContract.functions.owner().call()
 
                     # 企業リストから、企業名を取得する
                     company_name = ''
                     for company in company_list:
-                        if to_checksum_address(company['address']) == owner_address:
+                        if to_checksum_address(
+                                company['address']) == owner_address:
                             company_name = company['corporate_name']
 
                     # 第三者認定（Sign）のイベント情報を検索する
                     event_filter = TokenContract.eventFilter(
                         'Sign', {
-                            'filter':{},
-                            'fromBlock':'earliest'
-                        }
-                    )
+                            'filter': {},
+                            'fromBlock': 'earliest'
+                        })
                     try:
                         entries = event_filter.get_all_entries()
                     except:
@@ -155,33 +175,56 @@ class MyTokens(BaseResource):
                             isSigned = True
 
                         certification.append({
-                            'signer':entry['args']['signer'],
-                            'is_signed':isSigned
+                            'signer':
+                            entry['args']['signer'],
+                            'is_signed':
+                            isSigned
                         })
 
                     position_list.append({
                         'token': {
-                            'token_address': mytoken['token_address'],
-                            'token_template': token_template[1],
-                            'company_name': company_name,
-                            'name':name,
-                            'symbol':symbol,
-                            'totalSupply':totalSupply,
-                            'faceValue':faceValue,
-                            'interestRate':interestRate,
-                            'interestPaymentDate1':interestPaymentDate1,
-                            'interestPaymentDate2':interestPaymentDate2,
-                            'redemptionDate':redemptionDate,
-                            'redemptionAmount':redemptionAmount,
-                            'returnDate':returnDate,
-                            'returnAmount':returnAmount,
-                            'purpose':purpose,
-                            'image_url': [
-                                {'type': 'small', 'url': image_url_small},
-                                {'type': 'medium', 'url': image_url_medium},
-                                {'type': "large", 'url': image_url_large}
-                            ],
-                            'certification':certification
+                            'token_address':
+                            mytoken['token_address'],
+                            'token_template':
+                            token_template[1],
+                            'company_name':
+                            company_name,
+                            'name':
+                            name,
+                            'symbol':
+                            symbol,
+                            'totalSupply':
+                            totalSupply,
+                            'faceValue':
+                            faceValue,
+                            'interestRate':
+                            interestRate,
+                            'interestPaymentDate1':
+                            interestPaymentDate1,
+                            'interestPaymentDate2':
+                            interestPaymentDate2,
+                            'redemptionDate':
+                            redemptionDate,
+                            'redemptionAmount':
+                            redemptionAmount,
+                            'returnDate':
+                            returnDate,
+                            'returnAmount':
+                            returnAmount,
+                            'purpose':
+                            purpose,
+                            'image_url': [{
+                                'type': 'small',
+                                'url': image_url_small
+                            }, {
+                                'type': 'medium',
+                                'url': image_url_medium
+                            }, {
+                                'type': "large",
+                                'url': image_url_large
+                            }],
+                            'certification':
+                            certification
                         },
                         'balance': balance,
                         'commitment': commitment
@@ -198,7 +241,9 @@ class MyTokens(BaseResource):
         validator = Validator({
             'account_address_list': {
                 'type': 'list',
-                'schema': {'type': 'string'},
+                'schema': {
+                    'type': 'string'
+                },
                 'empty': False,
                 'required': True
             }
