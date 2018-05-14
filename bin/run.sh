a2747b2e5e9d7476de255ae92b029d57a38accfe
Yohei Kitagawa <y-kitagawa@nri.co.jp>
2018-05-14 10:55:29 +0900

    環境変数化

--- bin/run.sh
+++ bin/run.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 function start () {
     #source .venv/bin/activate
-    gunicorn -b 0.0.0.0:5000 --reload app.main:application --timeout 3000
+    gunicorn -b 0.0.0.0:5000 --reload app.main:application --timeout 3000 --workers=$WOKER_COUNT
 }
 
 function stop () {
     ps -ef | grep gunicorn | awk '{print $2}' | xargs kill -9
 }
 
+
 case "$1" in
     start)
-        start
+        start()
         ;;
     stop)
         stop
