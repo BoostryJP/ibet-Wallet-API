ef570527993f32725e1f61c7fcda3ffe8fe594f8
YoshihitoAso <yoshihito.asoh@gmail.com>
2018-12-03 07:58:34 +0900

    イベント検索をDB化

--- app/model/agreement.py
+++ app/model/agreement.py
@@ -9,10 +9,13 @@ from app.model import Base
 
 class Agreement(Base):
     __tablename__ = 'agreement'
+    id = Column(BigInteger, autoincrement=True)
     exchange_address = Column(String(256), primary_key=True)
     order_id = Column(BigInteger, primary_key=True)
     agreement_id = Column(BigInteger, primary_key=True)
     unique_order_id = Column(String(256), index=True)
+    buyer_address = Column(String(256), index=True)
+    seller_address = Column(String(256), index=True)
     counterpart_address = Column(String(256))
     amount = Column(BigInteger)
     status = Column(Integer)
@@ -22,10 +25,14 @@ class Agreement(Base):
             (self.exchange_address, self.order_id, self.agreement_id)
 
     FIELDS = {
+        'id': int,    
         'exchange_address': str,
         'order_id': int,
         'agreement_id': int,
-        'counterpart_address': int,
+        'unique_order_id': str,
+        'buyer_address': str,
+        'seller_address': str,
+        'counterpart_address': str,
         'amount': int,
         'status': int,
     }
--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ source ~/.bash_profile
 cd /app/tmr-node
 
 # async
-nohup python async/processor_OrderBook.py < /dev/null 2>&1 /dev/null &
+nohup python async/processor_OrderAgree.py < /dev/null 2>&1 /dev/null &
 
 #run server
 ./bin/run.sh start
