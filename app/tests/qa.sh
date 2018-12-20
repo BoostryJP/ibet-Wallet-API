01146fdbe4534ecdb6ae1f8b3828f5f0a9c10c5d
Yohei Kitagawa <y-kitagawa@nri.co.jp>
2018-12-20 16:41:56 +0900

    カバレッジ取得範囲設定

--- app/tests/qa.sh
+++ app/tests/qa.sh
@@ -4,7 +4,7 @@ source ~/.bash_profile
 cd /app/tmr-node
 
 # test
-pytest -v --cov --cov-report=xml --cov-branch
+pytest -v --cov=app/api/ --cov-report=xml --cov-branch
 
 # カバレッジファイルの移動
 mv coverage.xml cov/
