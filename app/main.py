736c628b4975f494562f4868f26c367fb0fa50fc
YoshihitoAso <yoshihito.asoh@gmail.com>
2019-06-06 21:36:15 +0900

    現在値、歩み値

--- app/main.py
+++ app/main.py
@@ -60,21 +60,23 @@ class App(falcon.API):
         self.add_route('/v1/Coupon/OrderBook', marketInformation.CouponOrderBook())
 
         # マーケット情報：現在値
+        self.add_route('/v1/JDR/LastPrice', marketInformation.JDRLastPrice())
         self.add_route('/v1/StraightBond/LastPrice', marketInformation.LastPrice())
         self.add_route('/v1/Membership/LastPrice', marketInformation.MembershipLastPrice())
         self.add_route('/v1/Coupon/LastPrice', marketInformation.CouponLastPrice())
 
         # マーケット情報：歩み値
+        self.add_route('/v1/JDR/Tick', marketInformation.JDRTick())
         self.add_route('/v1/StraightBond/Tick', marketInformation.Tick())
         self.add_route('/v1/Membership/Tick', marketInformation.MembershipTick())
         self.add_route('/v1/Coupon/Tick', marketInformation.CouponTick())
 
         # 保有トークン一覧
+        self.add_route('/v1/JDR/MyTokens', position.JDRMyTokens())
         self.add_route('/v1/StraightBond/MyTokens', position.MyTokens())
         self.add_route('/v1/Membership/MyTokens', position.MembershipMyTokens())
         self.add_route('/v1/Coupon/MyTokens', position.CouponMyTokens())
         self.add_route('/v1/CouponConsumptions', position.CouponConsumptions())
-        self.add_route('/v1/JDR/MyTokens', position.JDRMyTokens())
 
         # 保有トークン一覧（決済用）
         self.add_route('/v1/MRF/MyTokens', position.MRFMyTokens())
