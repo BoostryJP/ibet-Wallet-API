26823a57fa1bb5a6de4ed37d43797d3f3858d264
YoshihitoAso <yoshihito.asoh@gmail.com>
2019-06-23 02:51:29 +0900

    add swap abi

--- app/api/v1/nodeInfo.py
+++ app/api/v1/nodeInfo.py
@@ -1,4 +1,3 @@
-import os
 import json
 
 from app import config
@@ -7,34 +6,33 @@ from app.api.common import BaseResource
 
 LOG = log.get_logger()
 
+
 # ------------------------------
 # ノード情報
 # ------------------------------
 class NodeInfo(BaseResource):
-    '''
+    """
     Handle for endpoint: /v1/NodeInfo
-    '''
+    """
+
     def on_get(self, req, res):
         LOG.info('v1.nodeInfo.GetNodeInfo')
 
-        contracts = json.load(open('data/contracts.json' , 'r'))
+        contracts = json.load(open('data/contracts.json', 'r'))
 
         nodeInfo = {
             'payment_gateway_address': config.PAYMENT_GATEWAY_CONTRACT_ADDRESS,
             'payment_gateway_abi': contracts['PaymentGateway']['abi'],
-
             'personal_info_address': config.PERSONAL_INFO_CONTRACT_ADDRESS,
             'personal_info_abi': contracts['PersonalInfo']['abi'],
-
-            'ibet_exchange_address': config.IBET_SB_EXCHANGE_CONTRACT_ADDRESS,
-            'ibet_exchange_abi': contracts['IbetStraightBondExchange']['abi'],
-
+            'ibet_straightbond_exchange_address': config.IBET_SB_EXCHANGE_CONTRACT_ADDRESS,
+            'ibet_straightbond_exchange_abi': contracts['IbetStraightBondExchange']['abi'],
             'ibet_membership_exchange_address': config.IBET_MEMBERSHIP_EXCHANGE_CONTRACT_ADDRESS,
             'ibet_membership_exchange_abi': contracts['IbetMembershipExchange']['abi'],
-
             'ibet_coupon_exchange_address': config.IBET_CP_EXCHANGE_CONTRACT_ADDRESS,
             'ibet_coupon_exchange_abi': contracts['IbetCouponExchange']['abi'],
-
+            'ibet_depositaryreceipt_swap_address': config.IBET_JDR_SWAP_CONTRACT_ADDRESS,
+            'ibet_depositaryreceipt_swap_abi': contracts['IbetSwap']['abi'],
             'agent_address': config.AGENT_ADDRESS,
         }
 
