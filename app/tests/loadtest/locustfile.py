bedbc579dec85c9bdd67370b864698042507c859
YoshihitoAso <yoshihito.asoh@gmail.com>
2019-09-11 16:09:05 +0900

    loadtest for tsukubafes

--- app/tests/loadtest/locustfile.py
+++ app/tests/loadtest/locustfile.py
@@ -4,85 +4,106 @@ from __future__ import unicode_literals
 
 from locust import HttpLocust, TaskSet, task
 
-import os
 import json
-from app import config 
-from web3.auto import w3
-from eth_utils import decode_hex, to_checksum_address
+from app import config
 
 # テスト用のアカウント
-private_key = "0000000000000000000000000000000000000000000000000000000000000001"
-eth_address = "0x7E5F4552091A69125d5DfCb7b8C2659029395Bdf"
-
-# コントラクト情報
-contracts = json.load(open('../../../data/contracts.json' , 'r'))
-personalinfo_contract_address = \
-	to_checksum_address(config.PERSONAL_INFO_CONTRACT_ADDRESS)
-personalinfo_contract_abi = contracts['PersonalInfo']['abi']
+eth_address = "0x3509Ef81bAb3cf3B8d031119098871B56F08E80b"
 
 # Basic認証
 basic_auth_user = config.BASIC_AUTH_USER
 basic_auth_pass = config.BASIC_AUTH_PASS
 
+
 class LoadTestTaskSet(TaskSet):
-	def on_start(self):
-		self.client.get(
-			"/",
-			auth = (basic_auth_user, basic_auth_pass),
+    def on_start(self):
+        self.client.get(
+            "/",
+            auth=(basic_auth_user, basic_auth_pass),
+            verify=False
+        )
+
+    @task
+    def v1_jdr_mytokens(self):
+        headers = {'content-type': 'application/json'}
+        response = self.client.post(
+            "/v1/JDR/MyTokens/",
+            json.dumps({'account_address_list': [eth_address]}),
+            headers=headers,
+            auth=(basic_auth_user, basic_auth_pass),
+            verify=False
+        )
+        print(response.content)
+
+    @task
+    def v1_mrf_mytokens(self):
+        headers = {'content-type': 'application/json'}
+        response = self.client.post(
+            "/v1/MRF/MyTokens/",
+            json.dumps({'account_address_list': [eth_address]}),
+            headers=headers,
+            auth=(basic_auth_user, basic_auth_pass),
+            verify=False
+        )
+        print(response.content)
+
+    @task
+    def v1_requiredverion(self):
+        response = self.client.get(
+            "/v1/RequiredVersion/?platform=ios",
+            auth=(basic_auth_user, basic_auth_pass),
+            verify=False
+        )
+        print(response.content)
+
+    @task
+    def v1_orderlist(self):
+        headers = {'content-type': 'application/json'}
+        response = self.client.post(
+            "v1/OrderList",
+            json.dumps({'account_address_list': [eth_address]}),
+            headers=headers,
+			auth=(basic_auth_user, basic_auth_pass),
 			verify=False
-		)
+        )
+        print(response.content)
 
-	@staticmethod
-	def get_tx_info(self, eth_address):
-		response = self.client.get(
-			"/v1/Eth/TransactionCount/" + eth_address,
-			auth = (basic_auth_user, basic_auth_pass),
+    @task
+    def v1_jdr_lastprice(self):
+        headers = {'content-type': 'application/json'}
+        response = self.client.post(
+            "/v1/JDR/LastPrice",
+            json.dumps({'address_list': ["0xA4e577C0fb832a9643237E89e890B3e43F501b9e"]}), # テスト用DRトークン
+            headers=headers,
+			auth=(basic_auth_user, basic_auth_pass),
 			verify=False
-		)
-		response_json = json.loads(
-			response.content.decode('utf8').replace("'", '"'))
+        )
+        print(response.content)
 
-		nonce = response_json["data"]["nonce"]
-		gas_price = response_json["data"]["gasprice"]
+    @task
+    def v1_nodeinfo(self):
+        response = self.client.get(
+            "/v1/NodeInfo",
+            auth=(basic_auth_user, basic_auth_pass),
+            verify=False
+        )
+        print(response.content)
 
-		return nonce, gas_price
+    @task
+    def v1_jdr_contracts(self):
+        response = self.client.get(
+            "/v1/JDR/Contracts?limit=2",
+            auth=(basic_auth_user, basic_auth_pass),
+            verify=False
+        )
+        print(response.content)
 
-	@task
-	def v1_eth_sendrawtransaction(self):
-		contract = w3.eth.contract(
-			address = personalinfo_contract_address,
-			abi = personalinfo_contract_abi,
-		)
-
-		nonce, gas_price = LoadTestTaskSet.get_tx_info(self, eth_address)
-
-		txn = contract.functions.register(eth_address, '').buildTransaction({
-			'chainId': 2017,
-			'gas': 4000000,
-			'gasPrice': gas_price,
-			'nonce': nonce,
-		})
-
-		signed_txn = w3.eth.account.signTransaction(txn, private_key=private_key)
-
-		raw_tx_hex_list = []
-		raw_tx_hex_list.append(str(signed_txn.rawTransaction.hex()))
-
-		payload = {'raw_tx_hex_list': raw_tx_hex_list}
-		headers = {'content-type': 'application/json'}
-		response = self.client.post(
-			"/v1/Eth/SendRawTransaction",
-			headers = headers,
-			auth = (basic_auth_user, basic_auth_pass),
-			data = json.dumps(payload),
-			verify=False
-		)
-		print(response.content)
 
 class Website(HttpLocust):
-	task_set = LoadTestTaskSet
+    task_set = LoadTestTaskSet
 
-	# task実行の最短待ち時間
-	min_wait = 1000
-	# task実行の最大待ち時間
-	max_wait = 1000
+    # task実行の最短待ち時間
+    min_wait = 1000
+
+    # task実行の最大待ち時間
+    max_wait = 1000
