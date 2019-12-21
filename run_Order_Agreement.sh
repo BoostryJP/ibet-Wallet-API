75ff6bef12089972b716179c786bb50836419b21
YoshihitoAso <y-aso@boostry.co.jp>
2019-12-22 00:31:23 +0900

    fix: swap off

--- run_Order_Agreement.sh
+++ run_Order_Agreement.sh
@@ -3,5 +3,4 @@ source ~/.bash_profile
 
 cd /app/ibet-Wallet-API
 
-python async/processor_OrderAgree.py < /dev/null 2>&1 /dev/null &
-python async/processor_OrderAgree_Swap.py
+python async/processor_OrderAgree.py
