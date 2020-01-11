fe6a9a511d89ef62bf88e7a52a69f7c133872ad7
YoshihitoAso <y-aso@boostry.co.jp>
2020-01-12 02:04:12 +0900

    fix: standard output

--- run_Notifications.sh
+++ run_Notifications.sh
@@ -3,6 +3,6 @@ source ~/.bash_profile
 
 cd /app/ibet-Wallet-API
 
-python async/processor_Notifications.py < /dev/null 2>&1 /dev/null &
+python async/processor_Notifications.py &
 
 python async/processor_Notifications_bond.py
