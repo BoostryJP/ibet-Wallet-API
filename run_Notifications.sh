feaae8398403ec933884b723c8d70353ca91b647
YoshihitoAso <y-aso@boostry.co.jp>
2020-01-30 16:36:36 +0900

    feat: add initial offering notification

--- run_Notifications.sh
+++ run_Notifications.sh
@@ -3,6 +3,7 @@ source ~/.bash_profile
 
 cd /app/ibet-Wallet-API
 
+python async/processor_Notifications_Bond_Token.py &
 python async/processor_Notifications_Bond_Exchange.py &
 python async/processor_Notifications_Coupon_Exchange.py &
 python async/processor_Notifications_Membership_Exchange.py &
