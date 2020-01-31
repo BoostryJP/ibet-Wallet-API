a7f38551229e84ce15f979ea2e62ea846ba4727d
YoshihitoAso <y-aso@boostry.co.jp>
2020-01-31 12:43:40 +0900

    feat: add Coupon notification

--- run_Notifications.sh
+++ run_Notifications.sh
@@ -5,5 +5,6 @@ cd /app/ibet-Wallet-API
 
 python async/processor_Notifications_Bond_Token.py &
 python async/processor_Notifications_Bond_Exchange.py &
+python async/processor_Notifications_Coupon_Token.py &
 python async/processor_Notifications_Coupon_Exchange.py &
 python async/processor_Notifications_Membership_Exchange.py
