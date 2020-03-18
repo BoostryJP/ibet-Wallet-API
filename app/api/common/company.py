e6f66a4e35302316d32c7948a01be6ff3bc4d9db
Yoshihito Aso <y-aso@boostry.co.jp>
2020-03-18 13:36:51 +0900

    Revert "feat: add companyList API"

--- app/api/common/company.py
+++ app/api/common/company.py
@@ -10,7 +10,6 @@ from app.api.common import BaseResource
 from app.errors import AppError, InvalidParameterError, DataNotExistsError
 from app import config
 from app.contracts import Contract
-from app.model import Listing
 
 LOG = log.get_logger()
 
@@ -55,58 +54,6 @@ class CompanyInfo(BaseResource):
                 self.on_success(res, company_info)
         if not isExist:
             raise DataNotExistsError('eth_address: %s' % eth_address)
-
-
-# ------------------------------
-# 発行会社一覧参照
-# ------------------------------
-class CompanyInfoList(BaseResource):
-    """
-    Handle for endpoint: /v2/Companies
-    """
-
-    def on_get(self, req, res):
-        LOG.info('common.Company.CompanyInfoList')
-
-        session = req.context["session"]
-
-        # 会社リストを取得
-        try:
-            if config.APP_ENV == 'local':
-                company_list = json.load(open('data/company_list.json', 'r'))
-            else:
-                company_list = \
-                    requests.get(config.COMPANY_LIST_URL, timeout=config.REQUEST_TIMEOUT).json()
-        except Exception as err:
-            LOG.error('Failed To Get Data: %s', err)
-            raise AppError
-
-        # 取扱トークンリストを取得
-        available_tokens = session.query(Listing).all()
-
-        # TokenList-Contractへの接続
-        ListContract = Contract.get_contract(
-            'TokenList', config.TOKEN_LIST_CONTRACT_ADDRESS)
-
-        # 取扱トークンのownerAddressと会社リストを突合
-        listing_owner_list = []
-        for token in available_tokens:
-            token_address = to_checksum_address(token.token_address)
-            owner_address = ListContract.functions.getOwnerAddress(token_address).call()
-            listing_owner_list.append(owner_address)
-        has_listing_owner_function = self.has_listing_owner_function_creator(listing_owner_list)
-        filtered_company_list = filter(has_listing_owner_function, company_list)
-
-        self.on_success(res, list(filtered_company_list))
-
-    @staticmethod
-    def has_listing_owner_function_creator(listing_owner_list):
-        def has_listing_owner_function(company_info):
-            for address in listing_owner_list:
-                if company_info['address'] == address:
-                    return True
-            return False
-        return has_listing_owner_function
 
 
 # ------------------------------
