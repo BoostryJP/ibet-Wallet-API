e8f0ce509c74e3f950bef344ca3fd9c02689c92b
YoshihitoAso <y-aso@boostry.co.jp>
2020-04-01 13:40:30 +0900

    feat: Introduce github actions

--- app/tests/qa.sh
+++ app/tests/qa.sh
@@ -6,5 +6,11 @@ cd /app/ibet-Wallet-API
 # test
 pytest -v --cov=app/api/ --cov-report=xml --cov-branch
 
+status_code=$?
+
 # カバレッジファイルの移動
 mv coverage.xml cov/
+
+if [ $status_code -ne 0 ]; then
+  exit 1
+fi
