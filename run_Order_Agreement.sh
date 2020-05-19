c41e7291af9209cb41f06f99fa16cdcb741b09ee
YoshihitoAso <y-aso@boostry.co.jp>
2020-05-20 03:48:58 +0900

    fix

--- run_Order_Agreement.sh
+++ run_Order_Agreement.sh
@@ -5,8 +5,8 @@ source ~/.bash_profile
 cd /app/ibet-Wallet-API
 
 python async/indexer_Transfer.py &
-python async/indexer_OrderAgree.py &
+python async/indexer_OrderAgree.py
 
 if [ $COUPON_TOKEN_ENABLED = 1 ]; then
-  python async/indexer_Consume_Coupon.py &
+  python async/indexer_Consume_Coupon.py
 fi
