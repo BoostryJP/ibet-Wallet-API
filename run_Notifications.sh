d571f487e7960456c5be11e14338428086bba008
YoshihitoAso <y-aso@boostry.co.jp>
2020-05-19 23:07:38 +0900

    feat: Control processor startup for each token

--- run_Notifications.sh
+++ run_Notifications.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
+# shellcheck disable=SC1090
 source ~/.bash_profile
 
 cd /app/ibet-Wallet-API
 
-python async/processor_Notifications_Bond_Token.py &
-python async/processor_Notifications_Bond_Exchange.py &
-python async/processor_Notifications_Share_Token.py &
-python async/processor_Notifications_Share_Exchange.py &
-python async/processor_Notifications_Coupon_Token.py &
-python async/processor_Notifications_Coupon_Exchange.py &
-python async/processor_Notifications_Membership_Exchange.py
+if [ "$BOND_TOKEN_ENABLED" = 1 ]; then
+  python async/processor_Notifications_Bond_Token.py &
+  python async/processor_Notifications_Bond_Exchange.py &
+fi
+
+if [ "$SHARE_TOKEN_ENABLED" = 1 ]; then
+  python async/processor_Notifications_Share_Token.py &
+  python async/processor_Notifications_Share_Exchange.py &
+fi
+
+if [ "$MEMBERSHIP_TOKEN_ENABLED" = 1 ]; then
+  python async/processor_Notifications_Membership_Exchange.py
+fi
+
+if [ "$COUPON_TOKEN_ENABLED" = 1 ]; then
+  python async/processor_Notifications_Coupon_Token.py &
+  python async/processor_Notifications_Coupon_Exchange.py &
+fi
