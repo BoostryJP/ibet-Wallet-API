9b61ed8925216ee52514a1b895f746decf3992ed
YoshihitoAso <y-aso@boostry.co.jp>
2020-05-19 23:11:34 +0900

    feat: Control processor startup for each token

--- run_Order_Agreement.sh
+++ run_Order_Agreement.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+# shellcheck disable=SC1090
 source ~/.bash_profile
 
 cd /app/ibet-Wallet-API
 
 python async/indexer_Transfer.py &
 python async/indexer_OrderAgree.py &
-python async/indexer_Consume_Coupon.py
+
+if [ "$COUPON_TOKEN_ENABLED" = 1 ]; then
+  python async/indexer_Consume_Coupon.py &
+fi
