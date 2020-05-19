07ff7c3ffcb218419877375e6ffbe9a2a5bc54ee
YoshihitoAso <y-aso@boostry.co.jp>
2020-05-20 03:40:10 +0900

    fix

--- run_Notifications.sh
+++ run_Notifications.sh
@@ -5,20 +5,20 @@ source ~/.bash_profile
 cd /app/ibet-Wallet-API
 
 if [ $BOND_TOKEN_ENABLED = 1 ]; then
-  python async/processor_Notifications_Bond_Token.py &
-  python async/processor_Notifications_Bond_Exchange.py &
+  nohup python async/processor_Notifications_Bond_Token.py &
+  nohup python async/processor_Notifications_Bond_Exchange.py &
 fi
 
 if [ $SHARE_TOKEN_ENABLED = 1 ]; then
-  python async/processor_Notifications_Share_Token.py &
-  python async/processor_Notifications_Share_Exchange.py &
+  nohup python async/processor_Notifications_Share_Token.py &
+  nohup python async/processor_Notifications_Share_Exchange.py &
 fi
 
 if [ $MEMBERSHIP_TOKEN_ENABLED = 1 ]; then
-  python async/processor_Notifications_Membership_Exchange.py &
+  nohup python async/processor_Notifications_Membership_Exchange.py &
 fi
 
 if [ $COUPON_TOKEN_ENABLED = 1 ]; then
-  python async/processor_Notifications_Coupon_Token.py &
-  python async/processor_Notifications_Coupon_Exchange.py &
+  nohup python async/processor_Notifications_Coupon_Token.py &
+  nohup python async/processor_Notifications_Coupon_Exchange.py &
 fi
