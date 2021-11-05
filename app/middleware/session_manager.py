373144a1d4207f6112de46bcc1f612570d30c148
urushibata-k <urushibata@solidcom.co.jp>
2021-11-05 17:14:08 +0900

    feat: upgrade falcon

--- app/middleware/session_manager.py
+++ app/middleware/session_manager.py
@@ -33,28 +33,28 @@ class DatabaseSessionManager(object):
         self._session_factory = db_session
         self._scoped = isinstance(db_session, scoping.ScopedSession)
 
-    def process_request(self, req, res, resource=None):
+    def process_request(self, req, res):
         """
         Handle post-processing of the response (after routing).
         """
         req.context['session'] = self._session_factory
 
-    def process_response(self, req, res, resource=None):
+    def process_response(self, req, res, resource=None, req_succeeded=None):
         """
         Handle post-processing of the response (after routing).
         """
-        session = req.context['session']
+        session = req.context.get('session')
+        if session is not None:
+            if config.DB_AUTOCOMMIT:
+                try:
+                    session.commit()
+                except SQLAlchemyError as ex:
+                    session.rollback()
+                    raise DatabaseError(ERR_DATABASE_ROLLBACK, ex.args, ex.params)
 
-        if config.DB_AUTOCOMMIT:
-            try:
-                session.commit()
-            except SQLAlchemyError as ex:
-                session.rollback()
-                raise DatabaseError(ERR_DATABASE_ROLLBACK, ex.args, ex.params)
-
-        if self._scoped:
-            # remove any database-loaded state from all current objects
-            # so that the next access of any attribute, or any query execution will retrieve new state
-            session.remove()
-        else:
-            session.close()
+            if self._scoped:
+                # remove any database-loaded state from all current objects
+                # so that the next access of any attribute, or any query execution will retrieve new state
+                session.remove()
+            else:
+                session.close()
