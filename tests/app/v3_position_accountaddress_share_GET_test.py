018721ff281043af714b1ce5cadb6f73852472d1
YoshihitoAso <y-aso@boostry.co.jp>
2022-01-18 18:07:21 +0900

    fix

--- tests/app/v3_position_accountaddress_share_GET_test.py
+++ tests/app/v3_position_accountaddress_share_GET_test.py
@@ -608,17 +608,13 @@ class TestPositionAccountAddressShare:
             "code": 88,
             "message": "Invalid Parameter",
             "description": {
-                'code': 88,
-                'message': 'Invalid Parameter',
-                'description': {
-                    'limit': [
-                        "field 'limit' cannot be coerced: invalid literal for int() with base 10: 'test'",
-                        'must be of integer type'
-                    ],
-                    'offset': [
-                        "field 'offset' cannot be coerced: invalid literal for int() with base 10: 'test'",
-                        'must be of integer type'
-                    ]
-                }
+                'limit': [
+                    "field 'limit' cannot be coerced: invalid literal for int() with base 10: 'test'",
+                    'must be of integer type'
+                ],
+                'offset': [
+                    "field 'offset' cannot be coerced: invalid literal for int() with base 10: 'test'",
+                    'must be of integer type'
+                ]
             }
         }
