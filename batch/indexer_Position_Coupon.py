f1226b9aae79cf5b612c783be2d76f137194d402
YoshihitoAso <y-aso@boostry.co.jp>
2022-02-07 16:44:14 +0900

    Refactor error handling

--- batch/indexer_Position_Coupon.py
+++ batch/indexer_Position_Coupon.py
@@ -24,7 +24,7 @@ from itertools import groupby
 
 from eth_utils import to_checksum_address
 from sqlalchemy import create_engine
-from sqlalchemy.exc import OperationalError as SAOperationalError
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import Session
 
 path = os.path.join(os.path.dirname(__file__), "../")
@@ -521,8 +521,8 @@ def main():
             LOG.debug("Processed")
         except ServiceUnavailable:
             LOG.warning("An external service was unavailable")
-        except SAOperationalError:
-            LOG.error("Cannot connect to database")
+        except SQLAlchemyError as sa_err:
+            LOG.error(f"A database error has occurred: code={sa_err.code}\n{sa_err}")
         except Exception as ex:
             LOG.exception(ex)
 
