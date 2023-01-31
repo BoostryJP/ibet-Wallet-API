0ae802174b67ecac4e7071f3e8fa38a2001c156c
YoshihitoAso <yoshihito.asoh@gmail.com>
2023-01-31 11:57:07 +0900

    feat: Add Amazon SES option

--- bin/run_processor.sh
+++ bin/run_processor.sh
@@ -42,7 +42,7 @@ if [ "$EXCHANGE_NOTIFICATION_ENABLED" = 1 ]; then
   fi
 fi
 
-if [ ! -z "$SMTP_SERVER_HOST" ]; then
+if [ ! -z "$SMTP_METHOD" ]; then
   python batch/processor_Send_Mail.py &
 fi
 
