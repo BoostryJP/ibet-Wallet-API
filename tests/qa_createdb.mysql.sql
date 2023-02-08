bfa608f377c9779c67770d52560f87af80acb03f
Yosuke Otosu <y-otosu@boostry.co.jp>
2023-02-08 14:39:18 +0900

    fix: sql script for unit test

---
+++ tests/qa_createdb.mysql.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE apldb_gw0;
+CREATE DATABASE apldb_gw1;
+CREATE DATABASE apldb_gw2;
+CREATE DATABASE apldb_gw3;
+CREATE DATABASE apldb_gw4;
+
+GRANT ALL PRIVILEGES ON apldb_gw0.* TO 'apluser'@'%';
+GRANT ALL PRIVILEGES ON apldb_gw1.* TO 'apluser'@'%';
+GRANT ALL PRIVILEGES ON apldb_gw2.* TO 'apluser'@'%';
+GRANT ALL PRIVILEGES ON apldb_gw3.* TO 'apluser'@'%';
+GRANT ALL PRIVILEGES ON apldb_gw4.* TO 'apluser'@'%';
