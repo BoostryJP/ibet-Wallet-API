1a3f7dd3dc93bff960e9be77556a8dab5fbc73d8
Yosuke Otosu <y-otosu@boostry.co.jp>
2025-07-01 16:43:12 +0900

    Update transfer tests and models to include 'ibet_wst_bridge' message

--- app/model/db/idx_transfer.py
+++ app/model/db/idx_transfer.py
@@ -47,6 +47,7 @@ class DataMessage(BaseModel):
         "garnishment",
         "inheritance",
         "force_unlock",
+        "ibet_wst_bridge",
     ]
 
 
@@ -81,7 +82,7 @@ class IDXTransfer(Base):
     #   source_event = "Transfer"
     #     => None
     #   source_event = "Unlock"
-    #     => "force_unlock", "garnishment" or "inheritance"
+    #     => "force_unlock", "garnishment", "inheritance" or "ibet_wst_bridge"
     message: Mapped[str | None] = mapped_column(String(50), index=True)
 
     @staticmethod
